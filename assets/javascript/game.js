1fc950fc7b8b2fa33148f951ecd0aba4abd1d52a
d1son <dson120@gmail.com>
2017-03-09 19:24:55 -0500

    added js to show underscores for musicians

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,18 @@
 //src link this to html
 console.log("connected!") //connected to html
+
+var musicians = ["coldplay", "chainsmokers", "brunomars", "eminem"];
+
+var randomWordIndex = Math.floor(Math.random() *4);
+
+var randomMusician = musicians[randomWordIndex];
+
+var underscores = "";
+
+for (var i = randomMusician.length - 1; i >= 0; i--) {
+	underscores = underscores + " _ ";
+}
+
+
+$("#word").append(underscores);
+
